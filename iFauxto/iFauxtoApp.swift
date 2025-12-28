f472f0cd8afd12ee143904a4c6a74c4defa06e27
Jacob Valdez <jacobfv123@gmail.com>
2025-12-28 02:38:19 -0800

    Initial Commit

---
+++ iFauxto/iFauxtoApp.swift
@@ -0,0 +1,32 @@
+//
+//  iFauxtoApp.swift
+//  iFauxto
+//
+//  Created by Jacob Valdez on 12/28/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct iFauxtoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
